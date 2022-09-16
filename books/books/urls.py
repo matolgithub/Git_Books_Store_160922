6148b0a05e3e9f84f55d83eedb308d55bbe604bb
matolgithub <89175955650@mail.ru>
2022-09-16 21:58:43 +0300

    Next commit to git.

--- books/books/urls.py
+++ books/books/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from store.views import BookViewSet
+
+router = DefaultRouter()
+router.register('book', BookViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + router.urls
