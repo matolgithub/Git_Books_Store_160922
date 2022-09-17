e0ec885014bc64a6ef370ab2fd04ddc46d59acb7
matolgithub <89175955650@mail.ru>
2022-09-17 17:10:14 +0300

    next commit.

--- books/store/models.py
+++ books/store/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    author_name = models.CharField(max_length=255, default='Unknown author')
+
+    def __str__(self):
+        return f'ID: ---{self.name}, Name: ---{self.name}, Price: ---{self.price}, Author: ---{self.author_name}'
